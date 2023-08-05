3e1dcfaebef10cf4128b987eb2bbb09509d43eec
Ezekiel McCracken <theemccracken@icloud.com>
2023-08-04 23:28:47 -0400

    Initial commit, added Chakra UI, Chakra UI color mode, default system theme for light/dark mode in Chakra UI color mode and added boilerplate Clerk authentication code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '@/theme'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider {...pageProps}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ClerkProvider>
+  )
 }
